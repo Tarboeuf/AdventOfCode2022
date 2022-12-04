70e6dcaf1d341cb63ee24bf3b317d3bffe42df4b
Tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2022-12-04 09:34:52 +0100

    day4

---
+++ AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC.Day4
+{
+
+    [Day(ExpectedValue = "4")]
+    public class Day4Puzzle2 : IDay
+    {
+
+        public string GetPuzzle(string input)
+        {
+            return input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(GetAllValues).ToArray())
+                .Where(v => v[0].Intersect(v[1]).Count() >= 1)
+                .Count().ToString();
+        }
+
+
+        private HashSet<int> GetAllValues(string range)
+        {
+            var split = range.Split('-');
+            HashSet<int> result = new HashSet<int>();
+            for(int i = int.Parse(split[0]); i <= int.Parse(split[1]); i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+    }
+
+    [Day(ExpectedValue = "2")]
+    public class Day4Puzzle1 : IDay
+    {
+        public string GetPuzzle(string input)
+        {
+            return input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(GetAllValues).ToArray())
+                .Where(v => v[0].Intersect(v[1]).Count() == Math.Min(v[0].Count, v[1].Count))
+                .Count().ToString();
+        }
+
+        private HashSet<int> GetAllValues(string range)
+        {
+            var split = range.Split('-');
+            HashSet<int> result = new HashSet<int>();
+            for(int i = int.Parse(split[0]); i <= int.Parse(split[1]); i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+    }
+}
